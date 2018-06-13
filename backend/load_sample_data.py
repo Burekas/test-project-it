1c5d6566ac1df1d555e7f1eda5fbcc4c820cdcc4
burecas <imadnix@gmail.com>
2018-06-13 10:54:45 +0300

    Load sample data: support variable csv file, extendable

--- backend/load_sample_data.py
+++ backend/load_sample_data.py
@@ -9,6 +9,16 @@ from pymongo import MongoClient
 class ImportCsv:
     '''Import data from csvfile into MongoDb database in tree structure with parent references'''
 
+    field_ignore = ('END_GRADE', 'DESCRIPTION')
+
+    @property
+    def field_map(self):
+        ''' Map special fields to processors'''
+        return {
+            'GRADE': self._get_grade_doc,
+            'FULL_CODE': self._get_tag_doc
+        }
+
     def __init__(self, csvfile: str, database: str, collection: str, client: MongoClient):
         self.csvfile = csvfile
         self.db = client[database]
@@ -16,18 +26,29 @@ class ImportCsv:
         self.insert_count = 0
         super().__init__()
 
-    def _getid_or_insert(self, name: str, parent: ObjectId, extra_data: dict=None) -> ObjectId:
-        # document data
-        data = {'name': name, 'parent': parent}
-        # add extra data if exists
-        if extra_data:
-            data.update(extra_data)
-        # try to find
-        document = self.collection.find_one(data, {'_id': 1})
+    def _prepare_node_doc(self, field_name, row):
+        if field_name in self.field_map:
+            return self.field_map.get(field_name)(row)
+        return {'name': row.get(field_name)}
+
+    def _get_grade_doc(self, row):
+        grade = row.get('GRADE')
+        grade_end = row.get('END_GRADE')
+        is_grade_pk = grade.lower() == 'pk'
+        grade_name = f'Grade {grade_end}' if is_grade_pk else f'Grade {grade}-{grade_end}'
+        return {'name': grade_name}
+
+    def _get_tag_doc(self, row):
+        return {'name': row.get('FULL_CODE'),
+                'description': row.get('DESCRIPTION')}
+
+    def _getid_or_insert(self, new_doc: dict, parent: ObjectId) -> ObjectId:
+        new_doc.update({'parent': parent})
+        document = self.collection.find_one(new_doc, {'_id': 1})
         if not document:
             # insert new one and return ID
             self.insert_count += 1
-            return self.collection.insert_one(data).inserted_id
+            return self.collection.insert_one(new_doc).inserted_id
         # Just return ID of existed document
         return document.get('_id')
 
@@ -38,23 +59,13 @@ class ImportCsv:
         with open(self.csvfile, newline='') as csvfile:
             reader = csv.DictReader(csvfile)
             for row in reader:
-                # STANDARD
-                # get_id or insert new standard
-                standard_id = self._getid_or_insert(name=row.get('STANDARD'), parent=None)
-                # GRADE
-                # prepare name for grade
-                grade = row.get('GRADE')
-                grade_end = row.get('END_GRADE')
-                is_grade_pk = grade.lower() == 'pk'
-                grade_name = f'Grade {grade_end}' if is_grade_pk else f'Grade {grade}-{grade_end}'
-                # get_id or insert new grade
-                grade_id = self._getid_or_insert(name=grade_name, parent=standard_id)
-                # LEARNING_DOMAIN
-                learning_domain_id = self._getid_or_insert(name=row.get('LEARNING_DOMAIN'),
-                                                           parent=grade_id)
-                # TAG
-                self._getid_or_insert(name=row.get('FULL_CODE'), parent=learning_domain_id,
-                                      extra_data={'description': row.get('DESCRIPTION')})
+                # set parent to None for new row
+                parent = None
+                for key in row.keys():
+                    if key in self.field_ignore:
+                        continue
+                    doc = self._prepare_node_doc(key, row)
+                    parent = self._getid_or_insert(new_doc=doc, parent=parent)
         # create an index on parent field
         self.collection.create_index('parent')
         print(f'Inserted {self.insert_count} nodes\nDone.')
